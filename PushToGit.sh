db0d38b09716e140ca339d2f6a52b7297b22a482
free-doughnuts <kc.sudoer@gmail.com>
2024-02-01 21:34:47 +0200

    Fixing Code

--- PushToGit.sh
+++ PushToGit.sh
@@ -6,23 +6,28 @@ RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
  
-echo -e "${RED}==========Executing 'git status' For You==========:${ENDCOLOR}"
+echo "${RED}==========Executing 'git status' For You==========${ENDCOLOR}"
 git status
 sleep 3s
 
-echo -e "${RED}==========Executing 'git commint -am' For You, Please add a commit==========:${ENDCOLOR}"
-read message
-
-echo -e "${RED}==========Adding the file For You==========:${ENDCOLOR}" 
+echo "${RED}==========Exeuting 'git add .' For You==========${ENDCOLOR}" 
 git add .
+sleep 3s
+
+echo "${RED}==========Executing 'git status' For You Again==========${ENDCOLOR}"
+git status
+sleep 3s
+
+echo "${RED}==========Executing 'git commint -am' For You, Please add a commit==========${ENDCOLOR}"
+read message
 git commit -am "$message"
 sleep 3s
 
-echo -e "${RED}==========FILES HAS BEEN COMMITED==========${ENDCOLOR}"
+echo "${RED}==========FILES HAS BEEN COMMITED==========${ENDCOLOR}"
 git status
 sleep 5s
 
-echo -e "${GREEN}==========Exeuting 'git push -f' For You==========${ENDCOLOR}"
+echo "${GREEN}==========Exeuting 'git push -f' For You==========${ENDCOLOR}"
 git push -f
 
 
